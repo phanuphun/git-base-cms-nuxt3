3d72f2b2f31734a30f0fdeffef070fe716ce60a5
parnuphun <81858246+parnuphun@users.noreply.github.com>
2024-09-12 18:01:37 +0700

    Fixed Markdown Style

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Content v2 Minimal Starter
 - Look at the [Content documentation](https://content.nuxt.com/) to learn more.
-- If you want to use github markdown css [repository](https://github.com/sindresorhus/github-markdown-css)
 ## Setup
 - Make sure to install the dependencies: `npm install`
 - Start the development server on http://localhost:3000 :`npm run dev`
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   components : true ,
-  css: [
-    // '~/assets/css/github-markdown-dark.css',
-    //'~/assets/css/github-markdown-light.css',
-    // '~/assets/css/github-markdown.css'
-  ],
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxt/image'],
   routeRules: {
     '/': { prerender: true }
