1357c2cb18cb86fa0a8cecfe19e7c660167bd14f
parnuphun <81858246+parnuphun@users.noreply.github.com>
2024-11-07 21:51:04 +0700

    check recore corlor 4

--- pages/notes/[...tool].vue
+++ pages/notes/[...tool].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { toolIcons, type IconValue } from '~/model/iConInterface';
 const { tool } = useRoute().params
 const { data } = await useAsyncData('note', () =>
    queryContent('/notes')
@@ -6,82 +7,12 @@ const { data } = await useAsyncData('note', () =>
       .sort({ date: -1 })
       .find()
 )
+
+let toolIconsList:Record<string,IconValue>
 onMounted(() => {
-   console.log(tool);
+   toolIconsList = toolIcons;
 })
 
-interface Colors {
-   border:string
-   bg:string
-   bg2:string
-   hover:string
-}
-
-interface IconValue {
-   icon:string
-   colors?:Colors
-}
-
-const iconColors:Record<string,Colors> = {
-   green:{
-      border:"border-green-500",
-      hover:"hover:bg-green-100",
-      bg:"bg-green-50",
-      bg2:"bg-green-100"
-   },
-   purple:{
-      border:"border-purple-500",
-      hover:"hover:bg-purple-100",
-      bg:"bg-purple-50",
-      bg2:"bg-purple-100"
-   },
-   sky:{
-      border:"border-sky-500",
-      hover:"hover:bg-sky-100",
-      bg:"bg-sky-50",
-      bg2:"bg-sky-100"
-   },
-   orange:{
-      border:"border-orange-500",
-      hover:"hover:bg-orange-100",
-      bg:"bg-orange-50",
-      bg2:"bg-orange-100"
-   },
-   yellow:{
-      border:"border-yellow-500",
-      hover:"hover:bg-yellow-100",
-      bg:"bg-yellow-50",
-      bg2:"bg-yellow-100"
-   },
-   red:{
-      border:"border-red-500",
-      hover:"hover:bg-red-100",
-      bg:"bg-red-50",
-      bg2:"bg-red-100"
-   },
-   pink:{
-      border:"border-pink-500",
-      hover:"hover:bg-pink-100",
-      bg:"bg-pink-50",
-      bg2:"bg-pink-100"
-   },
-}
-
-const toolIcons: Record<string, IconValue> = {
-   ASPDotNetCore:{icon:'devicon:dotnetcore',colors:iconColors["purple"]},
-   Csharp: {icon:'devicon:csharp',colors:iconColors["purple"]},
-   Docker: {icon:'devicon:docker',colors:iconColors["sky"]},
-   Git: {icon:'devicon:git',colors:iconColors["orange"]},
-   Javascript: {icon:'devicon:javascript',colors:iconColors["yellow"]},
-   Linux: {icon:'devicon:linux',colors:iconColors["yellow"]},
-   NPM: {icon:'devicon:npm',colors:iconColors["red"]},
-   Nuxt: {icon:'devicon:nuxtjs',colors:iconColors["green"]},
-   Prisma: {icon:'devicon:prisma',colors:iconColors["purple"]},
-   Tailwind: {icon:'devicon:tailwindcss',colors:iconColors["sky"]},
-   Typescript: {icon:'devicon:typescript',colors:iconColors["sky"]},
-   VsCode: {icon:'devicon:vscode',colors:iconColors["sky"]},
-   Vue: {icon:'devicon:vuejs',colors:iconColors["green"]},
-}
 </script>
 
 <template>
@@ -90,14 +21,14 @@ const toolIcons: Record<string, IconValue> = {
             [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
          <div v-for="note in data" :key="note._id" class="break-inside-avoid mb-5 whitespace-normal">
             <div class="w-full border-2 rounded-2xl"
-            :class="[toolIcons[note.tool].colors?.border]">
+            :class="[toolIconsList[note.tool].colors?.border]">
                <div class="w-full flex flex-row border-b-2 p-3 rounded-t-2xl"
-               :class="[toolIcons[note.tool].colors?.bg2,toolIcons[note.tool].colors?.border]">
+               :class="[toolIconsList[note.tool].colors?.bg2,toolIconsList[note.tool].colors?.border]">
                   <p class="px-2 text-xl font-semibold"> {{ note.title }}</p>
                </div>
                <div class="mt-4">
                   <div v-if="note.tag" class="w-full flex justify-start px-2 my-2 text-sm">
-                     <div class="px-3 py-1 rounded-full border" :class="[toolIcons[note.tool].colors?.border]">
+                     <div class="px-3 py-1 rounded-full border" :class="[toolIconsList[note.tool].colors?.border]">
                         #{{ note.tag }}
                      </div>
                   </div>
--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { toolIcons, type IconValue } from '~/model/iConInterface';
+
 
 useHead({
    title: "Phanuphun.na - Note",
@@ -6,86 +8,17 @@ useHead({
       { name: "description", content: "Phanuphun.na - Note" }
    ]
 })
-
-interface Colors {
-   border:string
-   bg:string
-   bg2:string
-   hover:string
-}
-
-interface IconValue {
-   icon:string
-   colors:Colors
-}
-
-const iconColors:Record<string,Colors> = {
-   green:{
-      border:"border-green-500",
-      hover:"hover:bg-green-100",
-      bg:"bg-green-50",
-      bg2:"bg-green-100"
-   },
-   purple:{
-      border:"border-purple-500",
-      hover:"hover:bg-purple-100",
-      bg:"bg-purple-50",
-      bg2:"bg-purple-100"
-   },
-   sky:{
-      border:"border-sky-500",
-      hover:"hover:bg-sky-100",
-      bg:"bg-sky-50",
-      bg2:"bg-sky-100"
-   },
-   orange:{
-      border:"border-orange-500",
-      hover:"hover:bg-orange-100",
-      bg:"bg-orange-50",
-      bg2:"bg-orange-100"
-   },
-   yellow:{
-      border:"border-yellow-500",
-      hover:"hover:bg-yellow-100",
-      bg:"bg-yellow-50",
-      bg2:"bg-yellow-100"
-   },
-   red:{
-      border:"border-red-500",
-      hover:"hover:bg-red-100",
-      bg:"bg-red-50",
-      bg2:"bg-red-100"
-   },
-   pink:{
-      border:"border-pink-500",
-      hover:"hover:bg-pink-100",
-      bg:"bg-pink-50",
-      bg2:"bg-pink-100"
-   },
-}
-
-const toolIcons: Record<string, IconValue> = {
-   ASPDotNetCore:{icon:'devicon:dotnetcore',colors:iconColors["purple"]},
-   Csharp: {icon:'devicon:csharp',colors:iconColors["purple"]},
-   Docker: {icon:'devicon:docker',colors:iconColors["sky"]},
-   Git: {icon:'devicon:git',colors:iconColors["orange"]},
-   Javascript: {icon:'devicon:javascript',colors:iconColors["yellow"]},
-   Linux: {icon:'devicon:linux',colors:iconColors["yellow"]},
-   NPM: {icon:'devicon:npm',colors:iconColors["red"]},
-   Nuxt: {icon:'devicon:nuxtjs',colors:iconColors["green"]},
-   Prisma: {icon:'devicon:prisma',colors:iconColors["purple"]},
-   Tailwind: {icon:'devicon:tailwindcss',colors:iconColors["sky"]},
-   Typescript: {icon:'devicon:typescript',colors:iconColors["sky"]},
-   VsCode: {icon:'devicon:vscode',colors:iconColors["sky"]},
-   Vue: {icon:'devicon:vuejs',colors:iconColors["green"]},
-}
+let toolIconsList:Record<string,IconValue>
+onMounted(()=>{
+   toolIconsList = toolIcons;
+})
 
 </script>
 
 <template>
    <div class="w-full min-h-[calc(100vh-200px)] ">
       <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-         <div v-for="(tool, key) in toolIcons" :key="key" class="w-full border-2 rounded-xl flex  items-center
+         <div v-for="(tool, key) in toolIconsList" :key="key" class="w-full border-2 rounded-xl flex  items-center
          duration-200 hover:-translate-y-0.5 cursor-pointer "
          :class="[tool.colors?.border , tool.colors?.bg , tool.colors?.hover]">
             <NuxtLink :to="`notes/${key}`" class="w-full">
